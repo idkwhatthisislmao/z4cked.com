58aa0e742bffa5db3b278b4e33896fb9a6f89022
fryingpan923 <132401987+idkwhatthisislmao@users.noreply.github.com>
2025-02-23 17:28:48 -0500

    Update scripts.lua

--- scripts.lua
+++ scripts.lua
@@ -1,1 +1,39 @@
+local ScriptsTable = {
+	[1] = {
+		Name = "Slippery",
+		Desc = "Makes it so when you press H, every part becomes slippery (toggle)",
+		Src = game:HttpGet('https://raw.githubusercontent.com/idkwhatthisislmao/roblo/refs/heads/main/slip.lua', true),
+	},
+	[2] = {
+		Name = "Jerk Tool (R6)",
+		Desc = "Funny little tool, its in the name",
+		Src = game:HttpGet("https://raw.githubusercontent.com/idkwhatthisislmao/roblo/refs/heads/main/jerkoff.lua")
+	},
+	[3] = {
+		Name = "Jerk Tool (R15)",
+		Desc = "Funny little tool, its in the name",
+		Src = game:HttpGet("https://raw.githubusercontent.com/idkwhatthisislmao/roblo/refs/heads/main/jerkoffr15.lua")
+	},
+	[4] = {
+		Name = "Infinite Yield",
+		Desc = "A popular admin command script",
+		Src = game:HttpGet("https://raw.githubusercontent.com/edgeiy/infiniteyield/master/source")
+	},
+	[5] = {
+		Name = "Freecam",
+		Desc = "Official roblox freecam script (press shift + p)",
+		Src = game:HttpGet("https://raw.githubusercontent.com/idkwhatthisislmao/roblo/refs/heads/main/freecam.lua")
+	},
+	[6] = {
+		Name = "Synergy Networks Bypasser",
+		Desc = "A popular chat bypass script",
+		Src = game:HttpGetAsync("https://raw.githubusercontent.com/Synergy-Networks/products/main/BetterBypasser/loader.lua")
+	},
+	[7] = {
+		Name = "Pool Chat (Black Cat)",
+		Desc = "Allows you to see peoples chats while playing pool",
+		Src = game:HttpGet("https://raw.githubusercontent.com/idkwhatthisislmao/roblo/refs/heads/main/poolchat.lua")
+	},
+};
 
+return ScriptsTable
