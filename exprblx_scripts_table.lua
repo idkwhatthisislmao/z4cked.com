7c93f7635f7502e75a964798b3636bdd8dbb815b
fryingpan923 <132401987+idkwhatthisislmao@users.noreply.github.com>
2025-02-24 20:21:05 -0500

    Update exprblx_scripts_table.lua

--- exprblx_scripts_table.lua
+++ exprblx_scripts_table.lua
@@ -1,35 +1,40 @@
 local ScriptsTable = {
 	[1] = {
+		Name = "TBC Avatar Panel",
+		Desc = "Makes it so when you press H, every part becomes slippery (toggle)",
+		Src = game:HttpGet('https://raw.githubusercontent.com/idkwhatthisislmao/roblo/refs/heads/main/exprblxblackcatasset.lua', true),
+	},
+	[2] = {
 		Name = "Slippery",
 		Desc = "Makes it so when you press H, every part becomes slippery (toggle)",
 		Src = game:HttpGet('https://raw.githubusercontent.com/idkwhatthisislmao/roblo/refs/heads/main/slip.lua', true),
 	},
-	[2] = {
+	[3] = {
 		Name = "Jerk Tool (R6)",
 		Desc = "Funny little tool, its in the name",
 		Src = game:HttpGet("https://raw.githubusercontent.com/idkwhatthisislmao/roblo/refs/heads/main/jerkoff.lua")
 	},
-	[3] = {
+	[4] = {
 		Name = "Jerk Tool (R15)",
 		Desc = "Funny little tool, its in the name",
 		Src = game:HttpGet("https://raw.githubusercontent.com/idkwhatthisislmao/roblo/refs/heads/main/jerkoffr15.lua")
 	},
-	[4] = {
+	[5] = {
 		Name = "Infinite Yield",
 		Desc = "A popular admin command script",
 		Src = game:HttpGet("https://raw.githubusercontent.com/edgeiy/infiniteyield/master/source")
 	},
-	[5] = {
+	[6] = {
 		Name = "Freecam",
 		Desc = "Official roblox freecam script (press shift + p)",
 		Src = game:HttpGet("https://raw.githubusercontent.com/idkwhatthisislmao/roblo/refs/heads/main/freecam.lua")
 	},
-	[6] = {
+	[7] = {
 		Name = "Synergy Networks Bypasser",
 		Desc = "A popular chat bypass script",
 		Src = game:HttpGetAsync("https://raw.githubusercontent.com/Synergy-Networks/products/main/BetterBypasser/loader.lua")
 	},
-	[7] = {
+	[8] = {
 		Name = "Pool Chat (Black Cat)",
 		Desc = "Allows you to see peoples chats while playing pool",
 		Src = game:HttpGet("https://raw.githubusercontent.com/idkwhatthisislmao/roblo/refs/heads/main/poolchat.lua")
