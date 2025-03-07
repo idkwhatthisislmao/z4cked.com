e78ea0388edba5a531d095fed3e4cd366205b52c
fryingpan923 <132401987+idkwhatthisislmao@users.noreply.github.com>
2025-03-06 19:50:11 -0500

    Create blackcatsounditems.lua

---
+++ blackcatsounditems.lua
@@ -0,0 +1,8 @@
+local equip = loadstring(game:HttpGet("https://raw.githubusercontent.com/idkwhatthisislmao/roblo/refs/heads/main/blackcatequip.lua"))()
+
+equip({
+    33070696,
+    305888394,
+    24112667,
+    24114402
+})
