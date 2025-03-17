e1e389eb968f9321fda61abc44bab9d1580fcdad
fryingpan923 <132401987+idkwhatthisislmao@users.noreply.github.com>
2025-03-17 19:41:29 -0400

    Delete blackcatsounditems.lua

--- blackcatsounditems.lua
+++
@@ -1,8 +0,0 @@
-local equip = loadstring(game:HttpGet("https://raw.githubusercontent.com/idkwhatthisislmao/roblo/refs/heads/main/blackcatequip.lua"))()
-
-equip({
-    33070696,
-    305888394,
-    24112667,
-    24114402
-})
