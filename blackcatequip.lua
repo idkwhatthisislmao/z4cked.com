1273537129acffd21ff2030097dafb71aa995b4c
fryingpan923 <132401987+idkwhatthisislmao@users.noreply.github.com>
2025-03-17 19:41:23 -0400

    Delete blackcatequip.lua

--- blackcatequip.lua
+++
@@ -1,9 +0,0 @@
-local event = game.ReplicatedStorage.Remotes.V2.PutOnAccessory
-
-local function equip(table)
-   for _, item in pairs(table) do
-         event:FireServer(item)
-    end
-end
-
-return equip
