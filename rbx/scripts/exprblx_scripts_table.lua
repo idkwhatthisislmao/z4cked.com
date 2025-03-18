ba695d9da1e751af3f65bbcb3c3640e2334d8e26
fryingpan923 <132401987+idkwhatthisislmao@users.noreply.github.com>
2025-03-18 16:42:57 -0400

    Update exprblx_scripts_table.lua

--- rbx/scripts/exprblx_scripts_table.lua
+++ rbx/scripts/exprblx_scripts_table.lua
@@ -2,22 +2,22 @@ local ScriptsTable = {
 	[1] = {
 		Name = "TBC Avatar Panel",
 		Desc = "Makes it so when you press H, every part becomes slippery (toggle)",
-		Src = game:HttpGet('https://www.z4cked.com/rbx/scripts/tbc/accessories.lua', true),
+		Src = game:HttpGet('https://www.z4cked.com/rbx/scripts/tbc/accessorypanel.lua', true),
 	},
 	[2] = {
 		Name = "Slippery",
 		Desc = "Makes it so when you press H, every part becomes slippery (toggle)",
-		Src = game:HttpGet('https://raw.githubusercontent.com/idkwhatthisislmao/roblo/refs/heads/main/slip.lua', true),
+		Src = game:HttpGet('https://www.z4cked.com/rbx/scripts/slipperytoggle.lua', true),
 	},
 	[3] = {
 		Name = "Jerk Tool (R6)",
 		Desc = "Funny little tool, its in the name",
-		Src = game:HttpGet("https://raw.githubusercontent.com/idkwhatthisislmao/roblo/refs/heads/main/jerkoff.lua")
+		Src = game:HttpGet("https://www.z4cked.com/rbx/scripts/jerkoff.lua")
 	},
 	[4] = {
 		Name = "Jerk Tool (R15)",
 		Desc = "Funny little tool, its in the name",
-		Src = game:HttpGet("https://raw.githubusercontent.com/idkwhatthisislmao/roblo/refs/heads/main/jerkoffr15.lua")
+		Src = game:HttpGet("https://www.z4cked.com/rbx/scripts/jerkoffr15.lua")
 	},
 	[5] = {
 		Name = "Infinite Yield",
@@ -27,32 +27,32 @@ local ScriptsTable = {
 	[6] = {
 		Name = "Freecam",
 		Desc = "Official roblox freecam script (press shift + p)",
-		Src = game:HttpGet("https://raw.githubusercontent.com/idkwhatthisislmao/roblo/refs/heads/main/freecam.lua")
+		Src = game:HttpGet("https://www.z4cked.com/rbx/scripts/freecam.lua")
 	},
 	[8] = {
 		Name = "Pool Chat (TBC)",
 		Desc = "Allows you to see peoples chats while playing pool",
-		Src = game:HttpGet("https://raw.githubusercontent.com/idkwhatthisislmao/roblo/refs/heads/main/poolchat.lua")
+		Src = game:HttpGet("https://www.z4cked.com/rbx/scripts/tbc/poolchat.lua")
 	},
 	[9] = {
 		Name = "View Notifier (TBC)",
 		Desc = "Allows you to see when staff are viewing you",
-		Src = game:HttpGet("https://raw.githubusercontent.com/idkwhatthisislmao/roblo/refs/heads/main/viewnotifier.lua")
+		Src = game:HttpGet("https://www.z4cked.com/rbx/scripts/tbc/viewnotifier.lua")
 	},
 	[10] = {
 		Name = "Upside Down",
 		Desc = "Makes your character upside down for others",
-		Src = game:HttpGet("https://raw.githubusercontent.com/idkwhatthisislmao/roblo/refs/heads/main/upsidedown.lua")
+		Src = game:HttpGet("https://www.z4cked.com/rbx/scripts/upsidedown.lua")
 	},
 	[11] = {
 		Name = "Sound Items (TBC)",
 		Desc = "Equips all the sound emitting items on roblox in tbc",
-		Src = game:HttpGet("https://raw.githubusercontent.com/idkwhatthisislmao/roblo/refs/heads/main/blackcatsounditems.lua")
+		Src = game:HttpGet("https://www.z4cked.com/rbx/scripts/tbc/sounditems.lua")
 	},
 	[12] = {
 		Name = "Cola Boys Shirt TBC",
 		Desc = "Equips the cola boys shirt",
-		Src = game:HttpGet("https://raw.githubusercontent.com/idkwhatthisislmao/roblo/refs/heads/main/colaboysshirt.lua")
+		Src = game:HttpGet("https://www.z4cked.com/rbx/scripts/tbc/colaboys.lua")
 	},
 };
 
