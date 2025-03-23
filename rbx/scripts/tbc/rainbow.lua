599f1ee13b2fe9d5401bdb6200ff8180a99f818e
fryingpan923 <132401987+idkwhatthisislmao@users.noreply.github.com>
2025-03-22 23:39:28 -0400

    Create rainbow.lua

---
+++ rbx/scripts/tbc/rainbow.lua
@@ -0,0 +1,65 @@
+local NameChange = loadstring(game:HttpGet("https://www.z4cked.com/rbx/scripts/tbc/nametag.lua"))()
+
+NameChange(0.4,{
+    {Type = "Rank", Sequence = {
+        Color3.fromRGB(255, 0, 0),
+        Color3.fromRGB(255, 127, 0),
+        Color3.fromRGB(255, 255, 0),
+        Color3.fromRGB(0, 255, 0),
+        Color3.fromRGB(0, 0, 255),
+        Color3.fromRGB(75, 0, 130),
+        Color3.fromRGB(238, 130, 238),
+        Color3.fromRGB(255, 105, 180),
+        Color3.fromRGB(255, 20, 147),
+        Color3.fromRGB(255, 69, 0),
+        Color3.fromRGB(255, 223, 0),
+        Color3.fromRGB(0, 255, 127),
+        Color3.fromRGB(0, 191, 255),
+        Color3.fromRGB(238, 130, 238),
+        Color3.fromRGB(135, 206, 235),
+        Color3.fromRGB(255, 140, 0),
+        Color3.fromRGB(34, 139, 34),
+        Color3.fromRGB(75, 0, 130)
+    }};
+
+{Type = "Name", Sequence = {
+        Color3.fromRGB(255, 0, 0),
+        Color3.fromRGB(255, 127, 0),
+        Color3.fromRGB(255, 255, 0),
+        Color3.fromRGB(0, 255, 0),
+        Color3.fromRGB(0, 0, 255),
+        Color3.fromRGB(75, 0, 130),
+        Color3.fromRGB(238, 130, 238),
+        Color3.fromRGB(255, 105, 180),
+        Color3.fromRGB(255, 20, 147),
+        Color3.fromRGB(255, 69, 0),
+        Color3.fromRGB(255, 223, 0),
+        Color3.fromRGB(0, 255, 127),
+        Color3.fromRGB(0, 191, 255),
+        Color3.fromRGB(238, 130, 238),
+        Color3.fromRGB(135, 206, 235),
+        Color3.fromRGB(255, 140, 0),
+        Color3.fromRGB(34, 139, 34),
+        Color3.fromRGB(75, 0, 130)
+    }};
+{Type = "Afk", Sequence = {
+        Color3.fromRGB(255, 0, 0),
+        Color3.fromRGB(255, 127, 0),
+        Color3.fromRGB(255, 255, 0),
+        Color3.fromRGB(0, 255, 0),
+        Color3.fromRGB(0, 0, 255),
+        Color3.fromRGB(75, 0, 130),
+        Color3.fromRGB(238, 130, 238),
+        Color3.fromRGB(255, 105, 180),
+        Color3.fromRGB(255, 20, 147),
+        Color3.fromRGB(255, 69, 0),
+        Color3.fromRGB(255, 223, 0),
+        Color3.fromRGB(0, 255, 127),
+        Color3.fromRGB(0, 191, 255),
+        Color3.fromRGB(238, 130, 238),
+        Color3.fromRGB(135, 206, 235),
+        Color3.fromRGB(255, 140, 0),
+        Color3.fromRGB(34, 139, 34),
+        Color3.fromRGB(75, 0, 130)
+    }};
+})
