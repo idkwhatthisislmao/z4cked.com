c77a4f08e69b85621e59879e66c5d3f7186f69af
fryingpan923 <132401987+idkwhatthisislmao@users.noreply.github.com>
2025-03-22 23:38:20 -0400

    Create exprblx_scripts_table_nerf.lua

---
+++ rbx/scripts/exprblx_scripts_table_nerf.lua
@@ -0,0 +1,49 @@
+local ScriptsTable = {
+	[1] = {
+		Name = "TBC Avatar Panel",
+		Desc = "Makes it so when you press H, every part becomes slippery (toggle)",
+		Src = game:HttpGet('https://www.z4cked.com/rbx/scripts/tbc/accessorypanel.lua', true),
+	},
+	[2] = {
+		Name = "Slippery",
+		Desc = "Makes it so when you press H, every part becomes slippery (toggle)",
+		Src = game:HttpGet('https://www.z4cked.com/rbx/scripts/slipperytoggle.lua', true),
+	},
+	[3] = {
+		Name = "Jerk Tool (R6)",
+		Desc = "Funny little tool, its in the name",
+		Src = game:HttpGet("https://www.z4cked.com/rbx/scripts/jerkoff.lua")
+	},
+	[4] = {
+		Name = "Jerk Tool (R15)",
+		Desc = "Funny little tool, its in the name",
+		Src = game:HttpGet("https://www.z4cked.com/rbx/scripts/jerkoffr15.lua")
+	},
+	[5] = {
+		Name = "Infinite Yield",
+		Desc = "A popular admin command script",
+		Src = game:HttpGet("https://raw.githubusercontent.com/edgeiy/infiniteyield/master/source")
+	},
+	[6] = {
+		Name = "Freecam",
+		Desc = "Official roblox freecam script (press shift + p)",
+		Src = game:HttpGet("https://www.z4cked.com/rbx/scripts/freecam.lua")
+	},
+	[8] = {
+		Name = "Pool Chat (TBC)",
+		Desc = "Allows you to see peoples chats while playing pool",
+		Src = game:HttpGet("https://www.z4cked.com/rbx/scripts/tbc/poolchat.lua")
+	},
+	[9] = {
+		Name = "View Notifier (TBC)",
+		Desc = "Allows you to see when staff are viewing you",
+		Src = game:HttpGet("https://www.z4cked.com/rbx/scripts/tbc/viewnotifier.lua")
+	},
+	[10] = {
+		Name = "Upside Down",
+		Desc = "Makes your character upside down for others",
+		Src = game:HttpGet("https://www.z4cked.com/rbx/scripts/upsidedown.lua")
+	},
+};
+
+return ScriptsTable
